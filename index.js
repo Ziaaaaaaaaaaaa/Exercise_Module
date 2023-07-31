6903a663277f0c50e498eaff782bec0058ab67d8
Ziaaaaaaaaaaaa <ziakhan29042003@gmail.com>
2023-07-31 15:47:42 +0200

    commit

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
-let {writeFile,readFile,appendFile} = require('fs')
+// let {writeFile,readFile,appendFile} = require('fs')
 
-writeFile('./data.txt', 'cats are my favourite', (err,data)=>{
-    if(!err)
-    console.log(data);
-})
+// writeFile('./data.txt', 'cats are my favourite', (err,data)=>{
+//     if(!err)
+//     console.log(data);
+// })
 
 
-appendFile('./data.txt', ' dogs almost killed my cats', (err,data)=>{
-    if(!err)
-    console.log(data);
-})
+// appendFile('./data.txt', ' dogs almost killed my cats', (err,data)=>{
+//     if(!err)
+//     console.log(data);
+// })
 
-readFile('./data.txt', 'utf-8', (err,data)=>{
-    if(!err)
-    console.log(data);
-})
+// readFile('./data.txt', 'utf-8', (err,data)=>{
+//     if(!err)
+//     console.log(data);
+// })
 
+const Person = require('./Person');
+
+const person1 = new Person('zia', 'khan', 'zia.khan@example.com');
+const person2 = new Person('justin', 'scholtz', 'justin.scholtz@example.com');
+const person3 = new Person('jade', 'de wet', 'jade.de wet@example.com');
+
+console.log(person1);
+console.log(person2);
+console.log(person3);
